e0d467d09fbb37e226988d6274a081ea35699700
Toothless <hyunjinney@naver.com>
2023-01-20 19:29:47 +0900

    [level 1] Title: 최소직사각형, Time: 0.60 ms, Memory: 5.04 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cpp
@@ -11,14 +11,8 @@ int solution(vector<vector<int>> sizes) {
     
     for(int i=0;i<sizes.size();i++)
     {
-        if(sizes[i][0]<=sizes[i][1])
-        {
-            int tmp = sizes[i][0];
-            sizes[i][0]=sizes[i][1];
-            sizes[i][1]=tmp;
-        }
-        if(max1<=sizes[i][0]) max1=sizes[i][0];
-        if(max2<=sizes[i][1]) max2=sizes[i][1];
+        max1 = max(max1, max(sizes[i][0],sizes[i][1]));
+        max2 = max(max2, min(sizes[i][0],sizes[i][1]));
     }
     return max1*max2;
 }
