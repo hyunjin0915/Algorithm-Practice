d924bad6cbd19a3d16cf7a9a116ad197625bb279
Toothless <hyunjinney@naver.com>
2023-06-13 19:19:02 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c;
+    cin>>c;
+    
+    for(int i=0;i<c;i++)
+    {
+        int a;
+        int b;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+}
