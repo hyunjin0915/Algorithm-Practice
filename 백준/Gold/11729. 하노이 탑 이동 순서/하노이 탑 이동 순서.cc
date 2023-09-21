9e6d18f89c178a982630b2be3530b751bb465886
Toothless <hyunjinney@naver.com>
2023-09-22 08:27:21 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 168 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -4,19 +4,21 @@ using namespace std;
 
 void hanoi(int n, int start, int end, int temp)
 {
-    if(n==1)
-        cout<<start<<" "<<end<<'\n';
+    if(n==1) cout<<start<<" "<<end<<'\n';
+    
     else
     {
         hanoi(n-1, start, temp, end);
         cout<<start<<" "<<end<<'\n';
         hanoi(n-1, temp, end, start);
     }
+    
 }
+
 int main()
 {
-    int num;
+    int num; 
     cin>>num;
-    cout<<(1<<num) -1 <<'\n';
-    hanoi(num, 1,3,2);
+    cout<<(1<<num)-1<<'\n';
+    hanoi(num, 1, 3,2);
 }
