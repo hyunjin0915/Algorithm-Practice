7687a310a007b7b99a3a8b6181dd7ec9f9dcc7e0
Toothless <hyunjinney@naver.com>
2023-10-15 17:31:23 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -1,43 +1,47 @@
 #include <iostream>
-#include <cmath>
-
+ 
+#define endl "\n"
+#define MAX 10010
 using namespace std;
-
-int main()
+ 
+int N;
+int Arr[MAX];
+int DP[MAX];
+ 
+int Max(int A, int B) { if (A > B) return A; return B; }
+ 
+void Input()
 {
-    int arr[10001]={0,};
-    long long dp[10001]={0,};
-
-    int n;
-    cin>>n;
-    
-    for(int i=1;i<=n;i++)
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> Arr[i];
+}
+ 
+void Solution()
+{
+    DP[1] = Arr[1];
+    DP[2] = DP[1] + Arr[2];
+    for (int i = 3; i <= N; i++)
     {
-        int num;
-        cin>>num;
-        arr[i] = num;
+        DP[i] = Max(DP[i - 3] + Arr[i - 1] + Arr[i], DP[i - 2] + Arr[i]);
+        DP[i] = Max(DP[i - 1], DP[i]);
     }
-    
-    
-    dp[1] = arr[1];
-    dp[2] = arr[1]+arr[2];
-    dp[3] = max(arr[1]+arr[3],arr[2]+arr[3]);
-    
-    int answer = 0;
-    for(int i=4;i<=n;i++)
-    {
-        if(i==4)
-        {
-            dp[i] = max(dp[i-2], arr[i-1] + dp[i-3]) + arr[i];
-            continue;
-        }
-        dp[i] = max(max(arr[i-1] + dp[i-3],dp[i-2]), arr[i-1]+dp[i-4]) + arr[i];
-
-    }
-    for(int i=1;i<=n;i++)
-    {
-        if(answer<dp[i]) answer = dp[i];
-    }
-    cout<<answer;
-    
+    cout << DP[N] << endl;
+}
+ 
+void Solve()
+{
+    Input();
+    Solution();
+}
+ 
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    //freopen("Input.txt", "r", stdin);
+    Solve();
+ 
+    return 0;
 }
