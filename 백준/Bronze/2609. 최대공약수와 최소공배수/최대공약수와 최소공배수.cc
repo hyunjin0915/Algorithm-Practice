9439b8234f58b3678c7cd817b16d701a48ec48dc
Toothless <hyunjinney@naver.com>
2023-11-05 22:55:59 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,30 +1,25 @@
 #include <iostream>
+//#include <algorithm>
 
 using namespace std;
 
-int gcd(int a,int b)
-{
-    if(b==0)
-        return a;
-    else
-        return gcd(b,a%b);
-}
 
+int gcd(int a, int b)
+{
+	if (b == 0)
+	{
+		return a;
+	}
+	return gcd(b, a%b);
+}
 int main()
 {
-    int N,M;
-    cin>>N>>M;
-    
-    if(N<M)
-    {
-        int temp;
-        temp = N;
-        N = M;
-        M = temp;
-    }
-    
-    int gcdNum = gcd(N,M);
-    int lcmNum = N*M/gcdNum;
-    cout<<gcdNum<<endl<<lcmNum<<endl;
-
+	int n, m;
+	cin >> n >> m;
+	if (n < m)
+	{
+		swap(n, m);
+	}
+	int a = gcd(n, m);
+	cout << a <<'\n' << n * m / a;
 }
