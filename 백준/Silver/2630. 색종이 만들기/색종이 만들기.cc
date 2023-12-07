bf2af9b375b4dd73f0111edca0df9284a1e30da2
Toothless <hyunjinney@naver.com>
2023-12-07 21:39:47 +0900

    [Silver II] Title: 색종이 만들기, Time: 4 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,53 +1,52 @@
 #include <iostream>
-
 using namespace std;
 
-int arr[129][129];
-int white = 0;
+int arr[128][128] = { 0, };
 int blue = 0;
+int white = 0;
 
-void fc(int x, int y, int size)
+void check(int x, int y, int size)
 {
-    bool cut = false;
-    int first_color = arr[x][y];
-    for(int i=x; i<x+size;i++)
-    {
-        for(int j=y;j<y+size;j++)
-        {
-            if(arr[i][j]!=first_color)
-            {
-                cut=true;
-                break;
-            }
-        }
-    }
-    if(cut)
-    {
-        fc(x,y,size/2);
-        fc(x,y+size/2,size/2);
-        fc(x+size/2,y,size/2);
-        fc(x+size/2,y+size/2,size/2);
-    }
-    else
-    {
-        if(first_color==1) blue++;
-        else white++;
-    }
+	bool cut = false;
+	int firstColor = arr[x][y];
+	for (int i = x; i < x+size; i++)
+	{
+		for (int j = y; j < y+size; j++)
+		{
+			if (firstColor != arr[i][j])
+			{
+				cut = true;
+				break;
+			}
+		}
+	}
+
+	if (cut)
+	{
+		check(x, y, size / 2);
+		check(x, y + size / 2, size / 2);
+		check(x + size / 2, y, size / 2);
+		check(x + size / 2, y + size / 2, size / 2);
+	}
+	else
+	{
+		if (firstColor == 1) blue++;
+		else white++;
+	}
 }
 
 int main()
 {
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            cin>>arr[i][j];
-        }
-    }
-    fc(0,0,n);
-    cout<<white<<'\n';
-    cout<<blue<<'\n';
-    return 0;
+	int k;
+	cin >> k;
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	check(0, 0, k);
+	cout << white << '\n' << blue << '\n';
+	return 0;
 }
