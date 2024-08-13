f2c2aad9ec194f6694cb44b60b4201972e927606
Toothless <hyunjinney@naver.com>
2024-08-13 18:19:24 +0900

    [level 2] Title: 피보나치 수, Time: 0.54 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> v;
+    v.resize(100001,0);
+    v[0]=0;
+    v[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        v[i]=(v[i-1]+v[i-2])% 1234567;
+    }
+    answer = v[n];
+    return (answer);
+}
