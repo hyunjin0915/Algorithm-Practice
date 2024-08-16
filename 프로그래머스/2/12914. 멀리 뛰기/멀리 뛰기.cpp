3cfb86b980f6c65ab2b19305fae4ad4a6ee2a303
Toothless <hyunjinney@naver.com>
2024-08-17 08:23:14 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 3.65 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    vector<int> v;
+    v.resize(2001,0);
+    v[1]=1;
+    v[2]=2;
+    for(int i=3;i<=n;i++)
+    {
+        v[i]=(v[i-1]+v[i-2])%1234567;
+    }
+    answer=v[n];
+    return answer;
+}
