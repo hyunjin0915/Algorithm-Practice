180f143514d468e543d59893c3b806817738c40e
Toothless <hyunjinney@naver.com>
2024-11-07 10:09:02 +0900

    [level 2] Title: 오픈채팅방, Time: 279.22 ms, Memory: 43.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include<sstream>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    map<string, string> name;
+    
+    vector<pair<string, string>> info;
+    for(auto a : record)
+    {
+        stringstream ss(a);
+        vector<string> words;
+        string word;
+        while(getline(ss, word, ' '))
+        {
+            words.push_back(word);
+        }
+        if(words[0]=="Enter" || words[0]=="Change")
+        {
+            name[words[1]]=words[2];
+        }
+        info.push_back(make_pair(words[0], words[1]));
+    }
+    for(int i=0;i<info.size();i++)
+    {
+        string ans = "";
+        ans += name[info[i].second];
+        ans += "님이 ";
+        if(info[i].first == "Enter")
+        {
+            ans += "들어왔습니다.";
+        }
+        else if(info[i].first == "Leave")
+        {
+            ans += "나갔습니다.";
+        }
+        else continue;
+        answer.push_back(ans);
+    }
+    return answer;
+}
