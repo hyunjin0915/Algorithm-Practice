42e5257f3d9fbb29d797cf4e952eb6b04b705269
Toothless <hyunjinney@naver.com>
2024-11-14 23:30:14 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 35.62 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+    for(int i=0;i<numbers.size();i++)
+    {
+        if(numbers[i] %2 ==0)
+            answer.push_back(numbers[i] +1);
+        else
+        {
+            long long b = 1;
+            while(true)
+            {
+                if((numbers[i] & b) == 0) break; 
+                b*=2;
+            }
+            answer.push_back(numbers[i] + (b/2));
+        }
+    }
+    return answer;
+}
