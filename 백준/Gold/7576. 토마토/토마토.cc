bca67d0fb647dfa51f4ead062a20e9cccafd3bb4
Toothless <hyunjinney@naver.com>
2025-04-18 16:05:17 +0900

    [Gold V] Title: 토마토, Time: 212 ms, Memory: 10100 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,73 +1,73 @@
+using namespace std;
 #include <iostream>
 #include <queue>
 
-using namespace std;
+int N, M;
+int arr[1001][1001] = {0, };
+queue<pair<int, int>> q;
 
-int m, n;
-int box[1001][1001] = { 0, };
-//int dist[1001][1001] = { 0, };
 
-int dx[4] = {1,-1,0,0};
-int dy[4] = {0,0,1,-1};
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {1,-1,0,0};
 
-queue <pair<int, int>> qq;
 int answer = 0;
 
-void bfs()
-{
-	while (!qq.empty())
-	{
-		pair<int, int> cur = qq.front();
-		qq.pop();
-		int aa = cur.first;
-		int bb = cur.second;
-		
-		for (int i = 0; i < 4; i++)
-		{
-			int na = aa + dx[i];
-			int nb = bb + dy[i];
-			if (na > 0 && nb > 0 && na <= n && nb <= m)
-			{
-				if (box[na][nb] == 0)
-				{
-					qq.push(make_pair(na, nb));
-					box[na][nb] = box[aa][bb] + 1;
-					//cout << box[na][nb]<<",";
-				}
-			}
-		}
-
-	}
-}
+void BFS();
 
 int main()
 {
-	cin >> m >> n;
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= m; j++)
-		{
-			int num;
-			cin >> num;
-			box[i][j] = num;
-			if(num==1)	qq.push(make_pair(i, j));
+    cin>>M>>N;
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+        {
+            cin>>arr[i][j];
+            if(arr[i][j]==1)
+            {
+                q.push({i,j});
+            }
+        }
+    }
+    BFS();
+    
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+        {
+            if(arr[i][j]==0)
+            {
+                cout<<-1;
+                return 0;
+            }
+            else answer = max(answer, arr[i][j]);
+        }
+    }
+    cout<<answer-1;
+    return 0;
+}
 
-		}
-	}
-	bfs();
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= m; j++)
-		{
-			if (box[i][j] == 0)
-			{
-				cout << -1;
-				return 0;
-			}
-			else if (answer < box[i][j])
-				answer = box[i][j];
-		}
-	}
-	cout << answer-1;
-	return 0;
+void BFS()
+{
+    while(!q.empty())
+    {
+        int fx = q.front().first;
+        int fy = q.front().second;
+        q.pop();
+        for(int i=0;i<4;i++)
+        {
+            int nx = fx+dx[i];
+            int ny = fy+dy[i];
+            if(nx<0||nx>=N || ny<0||ny>=M) continue;
+            
+                if(arr[nx][ny]==0)
+                {
+                    q.push({nx,ny});
+                    arr[nx][ny] = arr[fx][fy]+1;
+                }
+            
+            
+        }
+
+    }
+
 }
