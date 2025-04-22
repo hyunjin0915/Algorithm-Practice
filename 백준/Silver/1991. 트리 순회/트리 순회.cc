3cb1df30a6edffc6f94f4351384e454250c28d10
Toothless <hyunjinney@naver.com>
2025-04-22 15:54:19 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,59 +1,57 @@
+using namespace std;
 #include <iostream>
-using  namespace std;
+#include <vector>
 
-pair<char,char> arr[26];
+int N;
+pair<char, char> arr[26];
 
-void preOrder(char x) //전
+
+void preOrder(char x)
 {
-    if (x != '.')
+    if(x!='.')
     {
-        cout << x;
-        preOrder(arr[x - 'A'].first);
-        preOrder(arr[x - 'A'].second);
+        cout<<x;
+        preOrder(arr[x- 'A'].first);
+        preOrder(arr[x- 'A'].second);
     }
     else return;
 }
-void inOrder(char x) //중
+void inOrder(char x)
 {
-    if (x != '.')
+    if(x!='.')
     {
-        inOrder(arr[x - 'A'].first);
-        cout << x;
-        inOrder(arr[x - 'A'].second);
+        inOrder(arr[x- 'A'].first);
+        cout<<x;
+        inOrder(arr[x- 'A'].second);
     }
     else return;
 }
-void postOrder(char x) //
+void postOrder(char x)
 {
-    if (x != '.')
+    if(x!='.')
     {
-        postOrder(arr[x - 'A'].first);
-        postOrder(arr[x - 'A'].second);
-        cout << x;
+        postOrder(arr[x- 'A'].first);
+        postOrder(arr[x- 'A'].second);
+        cout<<x;
+    }
+    else return;
+}
 
-    }
-    else return;
-}
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int N;
-    cin >> N;
-    for (int i = 0; i < N; i++)
+    cin>>N;
+     
+    for(int i=0;i<N;i++)
     {
         char a,b,c;
-        cin >> a >> b >> c;
-
-        arr[(a - 'A')].first = b;
-        arr[(a - 'A')].second = c;
+        cin>>a>>b>>c;
+        arr[a - 'A'].first = b;
+        arr[a-'A'].second = c;
     }
     preOrder('A');
-    cout << '\n';
+    cout<<'\n';
     inOrder('A');
-    cout << '\n';
+    cout<<'\n';
     postOrder('A');
     
 }
