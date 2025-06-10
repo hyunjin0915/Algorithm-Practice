5a3cc9f5febe47365c6d842844a828d57c855996
Toothless <hyunjinney@naver.com>
2025-06-10 12:30:50 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,38 +1,90 @@
-#include <iostream>
-#include <queue>
+/******************************************************************************
 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 using namespace std;
+#include <iostream>
 
-int main()
+int arr[100001];
+int N;
+int heapCnt = 0;
+
+void Swap(int *a, int *b)
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int N;
-    cin>>N;
-    priority_queue<int, vector<int>, greater<>> pQ;
-    
-    int x;
-    
-    for(int i=0;i<N;i++)
-    {
-        cin>>x;
-        if(x==0)
+    int tmp = *a;
+    *a = *b;
+    *b= tmp;
+}
+
+void Add(int num)
+{
+    arr[++heapCnt] = num;
+      int child = heapCnt;
+        int parent = heapCnt/2;
+        while(arr[child] < arr[parent] && parent >=1 )
         {
-            if(pQ.empty())
-            {
-                cout<<0<<'\n';
-                continue;
-            }
-            cout<<pQ.top()<<'\n';
-            pQ.pop();
+            Swap(&arr[child], &arr[parent]);
+            child = parent;
+            parent = child/2;
         }
-        else
+    
+}
+
+int Pop()
+{
+    int root = arr[1];
+    Swap(&arr[1] , &arr[heapCnt]);
+    heapCnt-=1;
+    int parent = 1;
+    int child = parent * 2;
+    
+    if(child + 1 <= heapCnt)
+    {
+        child = (arr[child]<arr[child + 1])? child: child + 1;
+    }
+    
+    while(arr[parent] > arr[child] && child <= heapCnt)
+    {
+        Swap(&arr[child], &arr[parent]);
+        parent = child;
+        child = parent*2;
+        if(child + 1 <= heapCnt)
         {
-            pQ.push(x);
+            child = (arr[child]<arr[child + 1])? child: child + 1;
         }
     }
     
-    return 0;
+    return root;
+}
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    cin>>N;
+    while(N--)
+    {
+        int x;
+        cin>>x;
+        if(x==0)
+        {
+            if(heapCnt == 0)
+            {
+                cout<<0<<'\n';
+            }
+            else
+            {
+                cout<<Pop()<<'\n';
+            }
+        }
+        else
+        {
+            Add(x);
+        }
+    }
 }
