5eaaf1ac5ed7f1337cbaf0e8490d1a5df7d5ac91
Toothless <hyunjinney@naver.com>
2025-08-02 10:25:07 +0900

    Time: 835 ms (10.36%), Space: 12.6 MB (93.22%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> buckets;
+    int target;
+    vector<int> matchsticks;
+    bool makesquare(vector<int>& _matchsticks) {
+        int sum = 0;
+        matchsticks = _matchsticks;
+        buckets.resize(4, 0);
+        for(int s : matchsticks)
+        {
+            sum += s;
+        }
+        if(sum%4 != 0) return false;
+        target = sum/4;
+        return DFS(0);
+    }
+    bool DFS(int idx)
+    {
+        if(idx == matchsticks.size())
+        {
+            for(int a : buckets)
+            {
+                if(a != target) return false;
+            }
+            return true;
+        }
+        for(int i = 0;i<buckets.size();i++)
+        {
+            if(buckets[i]+ matchsticks[idx] > target)
+            {
+                continue;
+            }
+
+            buckets[i] += matchsticks[idx];
+            if(DFS(idx+1)) return true;
+            buckets[i] -= matchsticks[idx];
+
+            if(buckets[i]==0) break;
+        }
+        return false;
+    }
+};
