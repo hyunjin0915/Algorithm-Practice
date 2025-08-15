146344cb2af6a7e2834c3ba468a5dd8ba734abf1
Toothless <hyunjinney@naver.com>
2025-08-15 11:54:55 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,53 +1,40 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-int s;
-vector<vector<int>> v;
+vector<vector<int>> g;
+vector<int> subtree;
 
-int BFS(int a, int b)
+int answer = -1;
+int n;
+
+int DFS(int u, int parent)
 {
-    vector<bool> visited(s+1, false);
-    visited[a] = true;
-    visited[b] = true;
-    
-    queue<int> q;
-    
-    q.push(a);
-    int cnt = 1;
-    while(!q.empty())
+    subtree[u]=1;
+    for(int w : g[u])
     {
-        int n = q.front();
-        q.pop();
-        
-        for(int m: v[n])
-        {
-            if(!visited[m]) 
-            {
-                q.push(m);
-                cnt++;
-                visited[m]=true;
-            }
-        }
+        if(w == parent) continue;
+        int cnt = DFS(w, u);
+        answer = min(answer, abs(cnt*2 - n));
+        subtree[u] += cnt;
     }
-    return cnt;
+    return subtree[u];
 }
 
-int solution(int n, vector<vector<int>> wires) {
-    int answer = n;
-    s = n + 1;
-    v.resize(n+1);
-    for(int i=0;i<wires.size();i++)
+int solution(int N, vector<vector<int>> wires) {
+    answer = N;
+    n=N;
+    g.resize(n+1);
+    subtree.resize(n+1);
+    
+    for(auto w : wires)
     {
-        v[wires[i][0]].push_back(wires[i][1]);
-        v[wires[i][1]].push_back(wires[i][0]);
+        g[w[0]].push_back(w[1]);
+        g[w[1]].push_back(w[0]);
     }
-    for(int i=0;i<wires.size();i++)
-    {
-        int cnt = BFS(wires[i][0], wires[i][1]);
-        answer = min(answer, abs(cnt*2 - n));
-    }
+    
+    DFS(1, 0);
+    
     return answer;
 }
