2ca3e595520d7cef5ce31965534139834f8b210b
Toothless <hyunjinney@naver.com>
2025-08-16 15:40:47 +0900

    [level 2] Title: 타겟 넘버, Time: 3.62 ms, Memory: 4.25 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,42 +1,35 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    
-    int index = 1;
-    int cnt = 2;
-   // int arr[1001]={0,};
-    queue<int> q;
-    q.push(numbers[0]);
-    q.push(-numbers[0]);
-    
-    while(index<numbers.size())
+int length, t;
+int answer = 0;
+
+vector<int> nums;
+
+void DFS(int depth, int count)
+{
+    if(depth == length) 
     {
-        for(int i=0; i<cnt;i++ )
+        if(count == t) 
         {
-            int front_node = q.front();
-            q.pop();
-            q.push(front_node + numbers[index]);
-            q.push(front_node - numbers[index]);
-            
-           // if(front_node - numbers[index]>0)   arr[front_node - numbers[index]]++;
-            //if(front_node + numbers[index]>0)   arr[front_node + numbers[index]]++;
-            
+            answer++;
         }
-        cnt*=2;
-        index++;
+        return;
     }
     
-    while(!q.empty())
-    {
-        int num = q.front();
-        if(num==target) answer++;
-        q.pop();
-    }
-   
+    DFS(depth+1, count + nums[depth]);
+    DFS(depth+1, count - nums[depth]);
+    
+}
+
+int solution(vector<int> numbers, int target) {
+    length = numbers.size();
+    t = target;
+    nums = numbers;
+    
+    DFS(0, 0);
+    
     return answer;
 }
